d1de6127eebfff810abd392ab33e2b7d699ce83e
daxkongjh <dax.k@live.com>
2022-07-31 00:40:06 +0800

    updated for mobile

--- Portfolio/src/components/Intro.jsx
+++ Portfolio/src/components/Intro.jsx
@@ -3,9 +3,11 @@ const Intro = ()=>{
         <div className="Intro">
                 <div className="textContainer">
                     <div className="css-typing">
-                    <p>"Hello World!".</p>
-                   <p> "I'm <span className="introName">Dax</span>. A Full-stack Developer"</p>
-                   <p> "Always Hungry. Always Improving"</p>
+                    <p>Hello World!</p>
+                   <p> I'm <span className="introHighlight">Dax</span>.</p>
+                   <p> A Full-stack <span className="introHighlight">Developer</span>.</p>
+                   <p> Always Hungry.</p>
+                   <p> Always Improving.</p>
                     </div>
                 </div>
             </div>
